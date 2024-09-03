5ef580df290bfb2345efe22b6992afe00b8a1e4d
YOGEESH K P <yogeesh793@gmail.com>
2024-09-03 16:33:43 +0530

    Add files via upload

---
+++ Numeric_operator.py
@@ -0,0 +1,26 @@
+# Numeric operator
+a=10
+b=5
+# addition
+print("a+b:",a+b)
+# subtraction
+print("a-b:",a-b)
+# division
+print("a/b:",a/b)
+# modulus
+print("a%b:",a%b)
+# exponent
+print("a**b:",a**b)
+# multiplication
+print("a*b",a*b)
+# floor division
+print("a//b:",a//b)
+# floor division exponent
+a//=3
+print("a//=3:",a)
+# remainder assignment
+a%=2
+print("a%=2:",a)
+# exponent assignment
+a**=3
+print("a**=3:",a)
